97628be9494f7a326cac6e45be0fed01951b58d9
roynys_pc <vadinload56@gmail.com>
2025-06-24 12:50:37 +0300

    дз2

--- app/src/main/java/com/example/playlist_maker_main/MainActivity.kt
+++ app/src/main/java/com/example/playlist_maker_main/MainActivity.kt
@@ -15,6 +15,21 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val search_btn = findViewById<Button>(R.id.search)
+        val library_btn = findViewById<Button>(R.id.library)
+        val settings_btn = findViewById<Button>(R.id.settings)
 
+        val searchClickListener: View.OnClickListener = object : View.OnClickListener{
+            override fun onClick(v: View?) {
+                Toast.makeText(this@MainActivity, "Нажал на поиск!", Toast.LENGTH_SHORT).show()
+            }
+        }
+        search_btn.setOnClickListener(searchClickListener)
+        library_btn.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Нажал на медиатеку!", Toast.LENGTH_SHORT).show()
+        }
+        settings_btn.setOnClickListener{
+            Toast.makeText(this@MainActivity, "Нажал на настройки!",Toast.LENGTH_SHORT).show()
+        }
     }
 }
