23b819d6803d45986ec38d930108fd2d1dde2167
roynys_pc <vadinload56@gmail.com>
2025-06-24 12:49:23 +0300

    Initial commit

---
+++ app/src/main/java/com/example/playlist_maker_main/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.playlistmaker
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.playlist_maker_main.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
