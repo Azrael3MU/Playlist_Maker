ad804dffaac6677713c10715a912d792b761853e
roynys_pc <vadinload56@gmail.com>
2025-06-27 12:00:11 +0300

    дз3

--- app/src/main/java/com/example/playlist_maker_main/MainActivity.kt
+++ app/src/main/java/com/example/playlist_maker_main/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.playlistmaker
+package com.example.playlist_maker_main
 
 import android.content.Intent
 import android.os.Bundle
--- app/src/main/java/com/example/playlist_maker_main/SettingsActivity.kt
+++ app/src/main/java/com/example/playlist_maker_main/SettingsActivity.kt
@@ -1,6 +1,8 @@
 package com.example.playlist_maker_main
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +13,11 @@ class SettingsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_settings)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        val backBtn = findViewById<ImageView>(R.id.back_button)
+
+        backBtn.setOnClickListener{
+            val backIntent = Intent(this, MainActivity::class.java)
+            startActivity(backIntent)
         }
     }
 }
